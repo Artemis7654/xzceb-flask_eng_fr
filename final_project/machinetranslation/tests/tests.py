3b0681c124812f2d6ba7d05b5ea2d3fd0dd97ecf
Artemis7654 <cristina.incas@gmail.com>
2023-06-07 05:59:50 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+
+sys.path.append('../machinetranslation')
+
+from machinetranslation import translator
+
+class Test_En_to_Fr(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(translator.english_to_french("Hello,"), "Bonjour,")
+    def test_Name(self):
+        self.assertNotEqual(translator.english_to_french("My name is Cristina"), "Je suis Cristina")
+
+class Test_Fr_to_En(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(translator.french_to_english("Bonjour"), "Hello")
+    def test_Name(self):
+        self.assertNotEqual(translator.french_to_english("Je suis Cristina"), "My name is Cristina")
+
+if __name__=='__main__':
+    unittest.main()
